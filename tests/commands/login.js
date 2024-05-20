dd6f6e23cfb6fecd7984fb59a187589e4f4031d1
kumpav <143574438+kumpav@users.noreply.github.com>
2024-05-20 17:18:21 +0530

    Update login.js

--- tests/commands/login.js
+++ tests/commands/login.js
@@ -1,7 +1,7 @@
 const { expect } = require('@playwright/test');
 
 async function testLogin(page){
-    await page.goto('https://qaomni.annalect.com/login', {timeout: 300});
+    await page.goto('https://qaomni.annalect.com/login', {timeout: 300000});
     await page.getByRole('button', { name: 'Sign in' }).click();
     await expect(page.getByRole('button', { name: 'Sign in' })).toBeVisible();
     await expect(page.locator('#customer-support')).toContainText('Request new account');
